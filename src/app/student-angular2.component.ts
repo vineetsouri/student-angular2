742852031157bbf5a0b979a766a0ca20e090ee6e
vineetsouri <vineetsouri72@gmail.com>
2016-06-30 14:32:36 +0530

    service code not working

--- src/app/student-angular2.component.ts
+++ src/app/student-angular2.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { StudentdbService } from '../app/studentdb.service';
 
 @Component({
   moduleId: module.id,
   selector: 'student-angular2-app',
   templateUrl: 'student-angular2.component.html',
-  styleUrls: ['student-angular2.component.css']
+  styleUrls: ['student-angular2.component.css'],
+  providers: [HTTP_PROVIDERS,StudentdbService]
 })
 export class StudentAngular2AppComponent {
+  
   title = 'student-angular2 works!';
 }
--- src/app/studentdb.service.ts
+++ src/app/studentdb.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
 
 @Injectable()
 export class StudentdbService {
 
-  constructor() {}
+  constructor(private http:Http) {};
 
+  private klassUrl: 'https://pure-retreat-73401.herokuapp.com/api/v1/klasses.json?access_token=TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4';
+  getKlasses (): Observable<Response> {
+    console.log("this.http.get(this.klassUrl)");
+    return this.http.get(this.klassUrl);
+    // .map(this.extractData).catch(this.handleError);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    console.log(body);
+    return body.klasses || [ ];
+  }
 }
+
+let p = new StudentdbService();
+p.getKlasses();
+console.log("StudentdbService.klassUrl");
